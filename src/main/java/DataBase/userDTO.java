8e5bce60f8b2bce7c9e126ba38b2691c90a4e418
Osama-Almostafa <61210365+Osama-Almostafa@users.noreply.github.com>
2020-12-02 10:15:06 +0100

    create user, appointment tilføjet

---
+++ src/main/java/DataBase/userDTO.java
@@ -0,0 +1,22 @@
+package DataBase;
+
+public class userDTO {
+    private String cpr;
+   private String paswd;
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+
+    public String getPaswd() {
+        return paswd;
+    }
+
+    public void setPaswd(String paswd) {
+        this.paswd = paswd;
+    }
+}
